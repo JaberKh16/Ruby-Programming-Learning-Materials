451d1d1800fb346653fe468becd305958059b5b8
Jaber Khan <md.jaberkhan66@gmail.com>
2025-12-14 22:50:00 +0600

    ruby-basic-program

---
+++ 1.Basic-Ruby-Program/1.first-program.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+
+# Ruby Programming Language
+# First Ruby Program
+puts "Hello, World!" # print the text with a new line
+print "This is my first Ruby program." # print the text without a new line
