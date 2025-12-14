6e721de3c84b66a5d9e76e35351aa0a780422209
Jaber Khan <md.jaberkhan66@gmail.com>
2025-12-15 04:06:04 +0600

    Ruby-Built-Function

---
+++ 3.Ruby-Built-In-Functon/1.math-function.rb
@@ -0,0 +1,16 @@
+# working with math functions in Ruby
+puts Math.sqrt(16)        # square root
+puts Math.sin(Math::PI/2) # sine of 90 degrees
+puts Math.cos(0)          # cosine of 0 degrees
+puts Math.tan(Math::PI/4) # tangent of 45 degrees
+puts Math.log(10)         # natural logarithm
+puts Math.log10(100)      # base-10 logarithm
+puts Math.exp(2)          # e raised to the power of 2
+puts Math.pow(2, 3)      # 2 raised to the power of 3
+puts Math.hypot(3, 4)    # hypotenuse of a right triangle
+puts Math.ceil(4.3)       # round up
+puts Math.floor(4.7)      # round down
+puts Math.atan2(1, 1)    # arctangent of y/x 
+puts Math.sinh(1)         # hyperbolic sine
+puts Math.cosh(1)         # hyperbolic cosine
+puts Math.tanh(1)         # hyperbolic tangent
