29ded3c1b6366a3f6b8ab5f32ff73d815076b25f
Jaber Khan <md.jaberkhan66@gmail.com>
2025-12-19 04:43:09 +0600

    Working-With-odules-Concepts-In-Ruby

--- 13.Modules-Concepts-In-Ruby/1.module-basic.rb
+++ 13.Modules-Concepts-In-Ruby/1.module-basic.rb
@@ -1,3 +1,14 @@
+=begin
+    Modules are basically the concepts of modularity why its can be think of a two things
+    which are - Namespaces and Mixins.
+
+    Interface => provides a contract what class can do
+    Mixins => provides a way to share (mix-in) ready code amoong multiple classes
+
+    Ruby Built-in modules like Enumerable and Comparable which can do the hard work
+    for you.
+=end
+
 # working with modules in Ruby
 module Greetings
     def self.hello(name)
--- 13.Modules-Concepts-In-Ruby/2.nested-module.rb
+++ 13.Modules-Concepts-In-Ruby/2.nested-module.rb
@@ -1,3 +1,61 @@
+=begin
+
+    Modules are basically the concepts of modularity why its can be think of a two things
+    which are - Namespaces and Mixins.
+
+    Namespaces => provides a namespaces which resides a class or function under its scope
+    Interface => provides a contract what class can do
+    Mixins => provides a way to share (mix-in) ready code amoong multiple classes
+
+    Example Of Namespaces:
+    ----------------------
+    module Toolbox
+      class Ruler
+        attr_accessor :length
+      end
+    end
+
+    module Country
+      class Ruler
+        attr_accessor :name
+      end
+    end
+
+    ruler1 = Toolbox::Ruler.new;  # Ruler:Class scopped to Toolbox:Module
+    ruler1.length = 20
+    puts ruler1.length # => 20
+
+    ruler2 = Country::Ruler.new;  # Ruler:Class scopped to Country:Module
+    ruler2.length = 20
+    puts ruler1.length # => 20
+
+
+    Example Of Mixins
+    -----------------
+    module NamingThing
+      attr_accessr :name
+
+      def print_name
+        puts "Name: #{@name}"
+      end
+    end
+
+    class Person
+      include NamingThing
+    end
+
+    person = Person.new;
+    person.name = "Joe"
+    person.print_name  # => Name: Joe
+
+
+    Ruby also built-in modules like Enumerable and Comparable which can do the hard work
+    for you.
+
+=end
+
+
+
 # working with nested module
 module Shop
   module Products
